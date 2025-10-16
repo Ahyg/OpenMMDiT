7cb69eec638d83ae3eeb838180e0e7ba243a614b
Yuguang Hu <auhuyg@gmail.com>
2025-10-16 18:06:36 +1100

    fixed dit channel bug

--- opendit/models/dit.py
+++ opendit/models/dit.py
@@ -60,7 +60,8 @@ class DiT(nn.Module):
         self.learn_sigma = learn_sigma
         self.use_video = use_video
         self.in_channels = in_channels
-        self.out_channels = in_channels * 2 if learn_sigma else in_channels
+        #self.out_channels = in_channels * 2 if learn_sigma else in_channels
+        self.out_channels = in_channels if learn_sigma else in_channels / 2
         self.hidden_size = hidden_size
         self.patch_size = patch_size
         self.input_size = input_size
